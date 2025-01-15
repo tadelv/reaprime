f9e7aee4aad8da3aaabdb1c86b9191c49a00cd09
Vid Tadel <tadelv@gmail.com>
2025-01-15 21:53:02 +0100

    permissions handler

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -8,6 +9,13 @@ import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
+
+Future<void> waitForPermission(bool wait, Future<void> Function() exec) async {
+  await Future.doWhile(() => wait);
+  wait = true;
+  await exec();
+  wait = false;
+}
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -26,6 +34,15 @@ class MyApp extends StatelessWidget {
     //
     // The ListenableBuilder Widget listens to the SettingsController for changes.
     // Whenever the user updates their settings, the MaterialApp is rebuilt.
+    var wait = false;
+    Future.forEach([
+      Permission.bluetoothScan.request,
+      Permission.bluetoothConnect.request,
+      Permission.bluetooth.request,
+      Permission.locationWhenInUse.request,
+      Permission.locationAlways.request,
+    ], (e) async => await waitForPermission(wait, e));
+
     return ListenableBuilder(
       listenable: settingsController,
       builder: (BuildContext context, Widget? child) {
