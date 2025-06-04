9ef89eb053e4ec8dddd6eab647126c04d2255ce0
Vid Tadel <tadelv@gmail.com>
2025-06-04 23:21:09 +0200

    lower ble verboseness

--- lib/src/models/device/impl/de1/de1.rw.dart
+++ lib/src/models/device/impl/de1/de1.rw.dart
@@ -15,8 +15,8 @@ extension De1ReadWrite on De1 {
 
   Future<void> _write(Endpoint e, Uint8List data) async {
     try {
-      _log.info('about to write to ${e.name}');
-      _log.info('payload: ${data.map((el) => toHexString(el))}');
+      _log.fine('about to write to ${e.name}');
+      _log.fine('payload: ${data.map((el) => toHexString(el))}');
       final characteristic = _service.characteristics
           .firstWhere((c) => c.characteristicUuid == Guid(e.uuid));
 
@@ -32,8 +32,8 @@ extension De1ReadWrite on De1 {
 
   Future<void> _writeWithResponse(Endpoint e, Uint8List data) async {
     try {
-      _log.info('about to write to ${e.name}');
-      _log.info('payload: ${data.map((el) => toHexString(el))}');
+      _log.fine('about to write with response to ${e.name}');
+      _log.fine('payload: ${data.map((el) => toHexString(el))}');
       final characteristic = _service.characteristics
           .firstWhere((c) => c.characteristicUuid == Guid(e.uuid));
 
