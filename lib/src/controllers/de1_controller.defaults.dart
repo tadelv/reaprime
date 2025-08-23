a1dbe09bb7080fa8379488a6f1c5974a0442fa85
Vid Tadel <tadelv@gmail.com>
2025-08-24 00:20:14 +0200

    quiet the fan

--- lib/src/controllers/de1_controller.defaults.dart
+++ lib/src/controllers/de1_controller.defaults.dart
@@ -1,9 +1,11 @@
 part of 'de1_controller.dart';
 
 extension Defaults on De1Controller {
-  _setDe1Defaults() async {
-	// TODO: set flush, steam and hotwater defaults
-	// TODO: set heater defaults
-	// TODO: send default/last profile
-	}
+  Future<void> _setDe1Defaults() async {
+    await _de1?.setFanThreshhold(55);
+
+    // TODO: set flush, steam and hotwater defaults
+    // TODO: set heater defaults
+    // TODO: send default/last profile
+  }
 }
