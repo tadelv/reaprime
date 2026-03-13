ebdd183b9c83214f3f6a772f1e26fdd2bd27229a
Vid Tadel <tadelv@gmail.com>
2026-03-13 14:49:24 +0100

    fix for InAppWebView on linux builds

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_inappwebview_linux/flutter_inappwebview_linux_plugin.h>
 #include <flutter_js/flutter_js_plugin.h>
 #include <flutter_libserialport/flutter_libserialport_plugin.h>
 #include <screen_retriever_linux/screen_retriever_linux_plugin.h>
@@ -15,9 +14,6 @@
 #include <window_manager/window_manager_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) flutter_inappwebview_linux_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterInappwebviewLinuxPlugin");
-  flutter_inappwebview_linux_plugin_register_with_registrar(flutter_inappwebview_linux_registrar);
   g_autoptr(FlPluginRegistrar) flutter_js_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterJsPlugin");
   flutter_js_plugin_register_with_registrar(flutter_js_registrar);
