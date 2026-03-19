3788fade652dc68ea32e0df0f8444e7b9f56b167
Vid Tadel <tadelv@gmail.com>
2026-03-19 07:29:33 +0100

    fix: switch to FlutterFragmentActivity for OnBackPressedCallback support
    FlutterActivity extends Activity directly which doesn't have
    onBackPressedDispatcher. FlutterFragmentActivity extends
    FragmentActivity -> ComponentActivity which provides it.
    Also fixes import: OnBackPressedDispatcher -> OnBackPressedCallback.

--- android/app/src/main/kotlin/net/tadel/reaprime/MainActivity.kt
+++ android/app/src/main/kotlin/net/tadel/reaprime/MainActivity.kt
@@ -11,12 +11,12 @@ import android.webkit.WebView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.core.content.FileProvider
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import java.io.File
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
     private val CHANNEL = "com.reaprime.updater/apk_installer"
     private val TAG = "MainActivity"
 
