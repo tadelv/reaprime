fef66c707076891d91540e2184b264d680a52b5c
Vid Tadel <tadelv@gmail.com>
2026-03-25 10:00:57 +0100

    cleanup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "net.tadel.reaprime"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
